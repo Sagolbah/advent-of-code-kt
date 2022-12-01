8995b40902bcbbe368f02c8e7a99d6880e344bb2
Daniil Boger <daniil.boger@jetbrains.com>
2022-12-02 01:01:55 +0400

    add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
+import kotlin.math.max
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun solve1(input: List<String>): Long {
+        var ans: Long = -1
+        var curr: Long = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                ans = max(ans, curr)
+                curr = 0
+            } else {
+                curr += line.toLong()
+            }
+        }
+        ans = max(ans, curr)
+        return ans
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun solve2(input: List<String>): Long {
+        val elves = mutableListOf<Long>()
+        var curr: Long = 0  // assume all input is > 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                elves.add(curr)
+                curr = 0
+            } else {
+                curr += line.toLong()
+            }
+        }
+        if (curr != 0L) {
+            elves.add(curr)
+        }
+        return elves.sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(solve1(input))
+    println(solve2(input))
 }
